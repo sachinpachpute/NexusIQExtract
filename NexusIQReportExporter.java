ed9612e6fef46f852cc4462c5290725ad12a5faa
sachinpachpute <78798010+sachinpachpute@users.noreply.github.com>
2024-02-12 14:30:54 +0000

    Update NexusIQReportExporter.java

--- NexusIQReportExporter.java
+++ NexusIQReportExporter.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -9,31 +10,39 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class NexusIQReportExporter {
 
-    public static void main(String[] args) {
-        String applicationPublicId = "your_application_public_id";
-        String reportId = "your_report_id";
-        String url = "http://localhost:8070/api/v2/applications/" + applicationPublicId + "/reports/" + reportId + "/policy";
+    private static final String NEXUS_IQ_URL = "https://nexus-iq.vocalink.co.uk";
+    private static final String APPLICATION_PUBLIC_ID = "acsw-xval";
+    private static final String REPORT_ID = "013c2f60ca4849f8813daa8473aa6431";
+    private static final String CSV_FILE_PATH = "NexusIQReportExporter_list.csv";
+    private static final String USERNAME = "tanisha.sethi";
+    private static final String PASSWORD = "V0calink2023!";
 
+    public static void main(String[] args) {
         try {
-            String responseJson = sendGetRequest(url);
+            String responseJson = sendGetRequest();
             List<PolicyDetail> policyDetails = extractPolicyDetails(responseJson);
-            writeToCSV(policyDetails, "policy_details.csv");
+            writeToCSV(policyDetails, CSV_FILE_PATH);
             System.out.println("CSV file created successfully.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String sendGetRequest(String url) throws IOException {
+    private static String sendGetRequest() throws IOException {
         HttpURLConnection connection = null;
         BufferedReader reader = null;
         StringBuilder response = new StringBuilder();
 
+
         try {
-            URL urlObj = new URL(url);
+            URL urlObj = new URL(NEXUS_IQ_URL + "/api/v2/applications/" + APPLICATION_PUBLIC_ID + "/reports/" + REPORT_ID + "/policy");
             connection = (HttpURLConnection) urlObj.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
+
+            String userCredentials = USERNAME + ":" + PASSWORD;
+            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
+            connection.setRequestProperty("Authorization", basicAuth);
 
             reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
@@ -56,44 +65,62 @@ public class NexusIQReportExporter {
         List<PolicyDetail> policyDetails = new ArrayList<>();
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode rootNode = objectMapper.readTree(responseJson);
-        JsonNode policiesNode = rootNode.path("policies");
+        JsonNode componentsNode = rootNode.path("components");
+        String applicationName = rootNode.path("application").path("name").asText();
 
-        for (JsonNode policyNode : policiesNode) {
-            String threatLevel = policyNode.path("threatLevel").asText();
-            String policyName = policyNode.path("policyName").asText();
-            JsonNode componentNode = policyNode.path("component");
+        for (JsonNode componentNode : componentsNode) {
             String componentDisplayName = componentNode.path("displayName").asText();
-            String applicationName = componentNode.path("application").path("name").asText();
-            policyDetails.add(new PolicyDetail(threatLevel, policyName, componentDisplayName, applicationName));
+            String policyName = null;
+            int threatLevel = 0;
+            String threatCategory = null;
+
+            JsonNode violationsNode = componentNode.path("violations");
+
+            if (violationsNode.isEmpty()){
+                continue;
+            }
+            for (JsonNode violationNode : violationsNode) {
+                threatLevel = violationNode.path("policyThreatLevel").asInt();
+                 policyName = violationNode.path("policyName").asText();
+                 threatCategory = violationNode.path("policyThreatCategory").asText();
+            }
+
+            policyDetails.add(new PolicyDetail(threatLevel, threatCategory, policyName, componentDisplayName, applicationName));
         }
         return policyDetails;
     }
 
     private static void writeToCSV(List<PolicyDetail> policyDetails, String fileName) throws IOException {
         try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
-            writer.println("Policy Threat level,Policy Name,Component Display Name,Application Name");
+            writer.println("Threat level,Threat Category, Policy Name,Component,Application");
             for (PolicyDetail detail : policyDetails) {
-                writer.println(detail.getThreatLevel() + "," + detail.getPolicyName() + "," +
+                writer.println(detail.getThreatLevel() + "," + detail.getThreatCategory() + ","+ detail.getPolicyName() + "," +
                         detail.getComponentDisplayName() + "," + detail.getApplicationName());
             }
         }
     }
 
     private static class PolicyDetail {
-        private String threatLevel;
+        private int threatLevel;
+        private String threatCategory;
         private String policyName;
         private String componentDisplayName;
         private String applicationName;
 
-        public PolicyDetail(String threatLevel, String policyName, String componentDisplayName, String applicationName) {
+        public PolicyDetail(int threatLevel, String threatCategory, String policyName, String componentDisplayName, String applicationName) {
             this.threatLevel = threatLevel;
+            this.threatCategory = threatCategory;
             this.policyName = policyName;
             this.componentDisplayName = componentDisplayName;
             this.applicationName = applicationName;
         }
 
-        public String getThreatLevel() {
+        public int getThreatLevel() {
             return threatLevel;
+        }
+
+        public String getThreatCategory() {
+            return threatCategory;
         }
 
         public String getPolicyName() {
