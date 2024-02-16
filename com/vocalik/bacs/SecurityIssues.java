3b2f2c18c3f1d68ba0d2d64b61c7d68264af6bf2
sachinpachpute <78798010+sachinpachpute@users.noreply.github.com>
2024-02-16 13:08:07 +0000

    Update SecurityIssues.java

--- com/vocalik/bacs/SecurityIssues.java
+++ com/vocalik/bacs/SecurityIssues.java
@@ -1,4 +1,37 @@
-package com.vocalik.bacs;
+package com.vocalink.bacs;
 
-public class SecurityIssues {
+public class SecurityIssue {
+    private String source;
+    private String reference;
+    private String severity;
+    private String threatCategory;
+    private String url;
+
+    public SecurityIssue(String source, String reference, String severity, String threatCategory, String url) {
+        this.source = source;
+        this.reference = reference;
+        this.severity = severity;
+        this.threatCategory = threatCategory;
+        this.url = url;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public String getThreatCategory() {
+        return threatCategory;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
