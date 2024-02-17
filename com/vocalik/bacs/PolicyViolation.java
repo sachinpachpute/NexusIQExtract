a71ab9eb30024b4f7ffec8fff2f5d46ed0f946c2
sachinpachpute <78798010+sachinpachpute@users.noreply.github.com>
2024-02-17 14:47:32 +0000

    Update PolicyViolation.java

--- com/vocalik/bacs/PolicyViolation.java
+++ com/vocalik/bacs/PolicyViolation.java
@@ -1,4 +1,37 @@
-package com.vocalik.bacs;
+package com.vocalink.bacs;
 
 public class PolicyViolation {
+    private String policyId;
+    private String policyName;
+    private String policyThreatCategory;
+    private int policyThreatLevel;
+    //private String waived;
+
+    public PolicyViolation(String policyId, String policyName, String policyThreatCategory, int policyThreatLevel) {
+        this.policyId = policyId;
+        this.policyName = policyName;
+        this.policyThreatCategory = policyThreatCategory;
+        this.policyThreatLevel = policyThreatLevel;
+        //this.waived = waived;
+    }
+
+    public String getPolicyId() {
+        return policyId;
+    }
+
+    public String getPolicyName() {
+        return policyName;
+    }
+
+    public String getPolicyThreatCategory() {
+        return policyThreatCategory;
+    }
+
+    public int getPolicyThreatLevel() {
+        return policyThreatLevel;
+    }
+
+    /*public String getWaived() {
+        return waived;
+    }*/
 }
