d4df483513face40ce526f50509e8fb2f080657b
sachinpachpute <78798010+sachinpachpute@users.noreply.github.com>
2024-02-17 14:46:19 +0000

    Update Coordinates.java

--- com/vocalik/bacs/Coordinates.java
+++ com/vocalik/bacs/Coordinates.java
@@ -1,4 +1,37 @@
-package com.vocalik.bacs;
+package com.vocalink.bacs;
 
 public class Coordinates {
+    private String format;
+    private String groupId;
+    private String artifactId;
+    private String version;
+    private String extension;
+
+    public Coordinates(String format, String groupId, String artifactId, String version, String extension) {
+        this.format = format;
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.version = version;
+        this.extension = extension;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
 }
