d3a886866fb9c0365410fa82dfe884f4abe69a6f
Sachin Pachpute <78798010+sachinpachpute@users.noreply.github.com>
2024-03-21 15:48:30 +0000

    Update PolicyViolation.java

--- com/vocalink/bacs/PolicyViolation.java
+++ com/vocalink/bacs/PolicyViolation.java
@@ -1,10 +1,14 @@
 package com.vocalink.bacs;
 
+import java.util.List;
+
 public class PolicyViolation {
     private String policyId;
     private String policyName;
     private String policyThreatCategory;
     private int policyThreatLevel;
+    private List<SecurityIssue> securityIssuesList;
+    private List<Constraint> constraintViolations;
     //private String waived;
 
     public PolicyViolation(String policyId, String policyName, String policyThreatCategory, int policyThreatLevel) {
@@ -29,6 +33,22 @@ public class PolicyViolation {
 
     public int getPolicyThreatLevel() {
         return policyThreatLevel;
+    }
+
+    public List<SecurityIssue> getSecurityIssuesList() {
+        return securityIssuesList;
+    }
+
+    public void setSecurityIssuesList(List<SecurityIssue> securityIssuesList) {
+        this.securityIssuesList = securityIssuesList;
+    }
+
+    public List<Constraint> getConstraintViolations() {
+        return constraintViolations;
+    }
+
+    public void setConstraintViolations(List<Constraint> constraintViolations) {
+        this.constraintViolations = constraintViolations;
     }
 
     /*public String getWaived() {
